b693511bee07f4239d146849c15df9921d2a1d7c
thyoondev <th.yoon.dev@gmail.com>
2020-06-10 15:57:14 +0900

    20200610

---
+++ 20200610/src/src/ez.java
@@ -0,0 +1,13 @@
+package src;
+
+public class ez {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "È«±æµ¿";
+		
+		System.out.println(name);
+		System.out.println("¹Ý°©½À´Ï´Ù ^^7");
+	}
+
+}
