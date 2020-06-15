c1b58ddc51fe47e579be42e05341b20a347ea37c
thyoondev <th.yoon.dev@gmail.com>
2020-06-15 15:46:53 +0900

    20200615

--- 20200615/src/Ex.java
+++ 20200615/src/Ex.java
@@ -3,18 +3,23 @@ public class Ex {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		J_0615_sub sub = new J_0615_sub();
+		
 		Scanner scanner = new Scanner(System.in);
 		
 		System.out.print("이름 : ");
 		String name = scanner.next();
 		System.out.print("국어 점수 : ");
-		int korean = scanner.nextInt();
+		int kor = scanner.nextInt();
 		System.out.print("수학 점수 : ");
-		int math = scanner.nextInt();
+		int mat = scanner.nextInt();
+		System.out.print("영어 점수 : ");
+		int eng = scanner.nextInt();
 		
-		int total = korean+math;
+		int total = sub.sum3(kor ,mat ,eng);
 		
-		int avg =  total/2;
+		J_0615_sub sub2 = new J_0615_sub();
+		int avg = sub2.ava(total, 3);
 		
 		System.out.println(name + "님의 총점은 : " + total + " 점 입니다.");
 		System.out.println(name + "님의 평균은 : " + avg + " 점 입니다.");
---
+++ 20200615/src/kyo.java
@@ -0,0 +1,18 @@
+
+public class kyo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int total = 200;
+		int count = 3;
+		int ava = total / count;
+		
+		
+		double dd = 200.0/3;
+		
+		System.out.println("int ЦђБе : " + ava);
+		System.out.println("double ЦђБе : " + dd);
+	}
+
+}
