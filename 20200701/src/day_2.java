601c0a1d1daca58db593d8670a6aa526dc42bd5d
thyoondev <th.yoon.dev@gmail.com>
2020-07-01 17:41:43 +0900

    20200701

--- 20200701/src/day_2.java
+++ 20200701/src/day_2.java
@@ -12,7 +12,7 @@ public class day_2 {
 		System.out.print("몇과목 ? :");
 		int listnum = sc.nextInt();
 		
-		String[] title = new String[listnum+4];
+		String[] title = new String[listnum+4];	//ex) listnum가 3일때 변수는 총 7칸 name/list1/list2/list3/total/avg/rank
 		String[] main = new String[listnum+4];
 		
 		title[0] = "성명";
@@ -20,33 +20,33 @@ public class day_2 {
 		title[title.length-2] = "평균";
 		title[title.length-1] = "등급";
 		
-		System.out.print("이름 ? :");
+		System.out.print("이름 ? :"); //이름 받기
 		String name = sc.next();
 		main[0] = name;
 		
-		for(int i = 1; i <= title.length-4; i++) {
+		for(int i = 1; i <= title.length-4; i++) { //과목명 받기
 			System.out.print(i + "번째 과목명 : ");
 			t_list = sc.next();
 			title[i] = t_list;
 		}
 		
-		for(int i = 1; i <= title.length-4; i++) {
-			System.out.print(title[i] + "점수 : ");
+		for(int i = 1; i <= title.length-4; i++) { //과목별 점수 받기
+			System.out.print(title[i] + " 점수 : ");
 			score = sc.nextInt();
-			total += score;
 			main[i] = Integer.toString(score);
 		}
 		
+		total = m.total(main);
 		main[main.length-3] = Integer.toString(total); //총점
 		main[main.length-2] = m.ava(total, listnum); //평균
 		main[main.length-1] = m.rank(Double.parseDouble(main[main.length-2])); //등급
 		
-		for(int i = 0; i < title.length; i++) {
+		for(int i = 0; i < title.length; i++) { //상단 출력
 			System.out.print(title[i]+"\t");	
 		}
 		System.out.println("");
 		
-		for(int i = 0; i < title.length; i++) {
+		for(int i = 0; i < title.length; i++) { //하단 출력
 			System.out.print(main[i]+"\t");
 		}
 
--- 20200701/src/math.java
+++ 20200701/src/math.java
@@ -2,13 +2,15 @@
 public class math {
 
 
-	String ava(int a, int b) {
-		String c = Double.toString(a/(double)b);
-		return c;
+	String ava(int a, int b) {	//평균 구하기
+		Double c = a/(double)b;
+		c = Math.round(c*100)/100.0;
+		String d = Double.toString(c);
+		return d;
 	}
 	
 	
-	String b;
+	String b; 		//평균 기준 등급 구하기
 	String rank(double a) {
 		if(a >= 90) {
 			b = "수";
@@ -24,5 +26,12 @@ public class math {
 		return b;
 	}
 	
+	int total = 0;	//총점 구하기
+	int total(String[] a) {
+		for(int i=1; i<=a.length-4;i++) {
+			total += Integer.parseInt(a[i]);
+		}
+		return total;
+	}
 	
 }
