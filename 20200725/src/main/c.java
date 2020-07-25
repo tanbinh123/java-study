e647fe4c5797009ca419bb4e548a880c03c6da92
thyoondev <th.yoon.dev@gmail.com>
2020-07-25 21:06:24 +0900

    20200725

---
+++ 20200725/src/main/c.java
@@ -0,0 +1,19 @@
+package main;
+
+public class c
+extends p
+implements inter
+{
+
+	@Override
+	public int abc() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int a() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
---
+++ 20200725/src/main/p.java
@@ -0,0 +1,6 @@
+package main;
+
+public abstract class p {
+
+	public abstract int abc();
+}
