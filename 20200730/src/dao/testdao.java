2807e8760781aaa34405d2958ff43a763d6d73e6
junh2 <th.yoon.dev@gmail.com>
2020-07-30 20:04:49 +0900

    20200730

--- 20200730/src/dao/testdao.java
+++ 20200730/src/dao/testdao.java
@@ -16,9 +16,9 @@ public class testdao {
 	PreparedStatement ps = null;
 	ResultSet rs = null;
 	
-	String query;
-	public ArrayList<testDB_dto> selectTestDB(){
-		
+	/*
+	//select문
+	public ArrayList<testDB_dto> selectTestDB(){	
 	ArrayList<testDB_dto> arr = new ArrayList<>();
 	String query = "select empno, ename, job from test";
 	try{
@@ -46,7 +46,27 @@ public class testdao {
 	return arr;
 }
 	
+	//update문
+	public ArrayList<testDB_dto> updateTestDB(int setEmpno, String setEname, String setJob, int where){
 	
+	ArrayList<testDB_dto> arr = new ArrayList<>();
+	String query = "update test set empno = "+setEmpno+", ename = '"+ setEname +"', job = '" + setJob+"' where empno ="+where;
+	try{
+		connection = common.getConnection();
+		ps = connection.prepareStatement(query);
+		rs = ps.executeQuery();
+
+	}catch(SQLException se){
+		System.out.println("getListAll() SQL query 오류" + query);
+	}catch(Exception e){
+		System.out.println("getListAll() 오류");
+	}finally {
+		common.close(connection, ps, rs);
+	}
+	
+	return arr;
+}
+	//insert문
 	public ArrayList<testDB_dto> insertTestDB(int setEmpno, String setEname, String setJob){
 	
 	ArrayList<testDB_dto> arr = new ArrayList<>();
@@ -55,16 +75,65 @@ public class testdao {
 		connection = common.getConnection();
 		ps = connection.prepareStatement(query);
 		rs = ps.executeQuery();
-//		
-//		while(rs.next()) {
-//			int empno = rs.getInt(1);//name
-//			String ename = rs.getString(2);//3
-//			String job = rs.getString(3);//4
-//			
-//			testDB_dto dto = new testDB_dto(empno, ename, job);
-//			arr.add(dto);
-//			
-//		}
+
+	}catch(SQLException se){
+		System.out.println("getListAll() SQL query 오류" + query);
+	}catch(Exception e){
+		System.out.println("getListAll() 오류");
+	}finally {
+		common.close(connection, ps, rs);
+	}
+	
+	return arr;
+}
+	//delete문
+	public ArrayList<testDB_dto> deleteTestDB(int setEmpno){
+	
+	ArrayList<testDB_dto> arr = new ArrayList<>();
+	String query = "delete from test where empno = " + setEmpno;
+	try{
+		connection = common.getConnection();
+		ps = connection.prepareStatement(query);
+		rs = ps.executeQuery();
+
+	}catch(SQLException se){
+		System.out.println("getListAll() SQL query 오류" + query);
+	}catch(Exception e){
+		System.out.println("getListAll() 오류");
+	}finally {
+		common.close(connection, ps, rs);
+	}
+	
+	return arr;
+}
+	*/
+	
+	
+	
+	
+	//select, insert, update, delete 통합
+	String query = "";
+	public ArrayList<testDB_dto> allControlTestDB(int gubun,int setEmpno, String setEname, String setJob, int where){
+	ArrayList<testDB_dto> arr = new ArrayList<>();
+	if (gubun == 1)query = "select empno, ename, job from test";
+	else if (gubun == 2)query = "insert into TEST VALUES ("+setEmpno+",'"+ setEname +"','" + setJob+"')";
+	else if (gubun == 3)query = "update test set empno = "+setEmpno+", ename = '"+ setEname +"', job = '" + setJob+"' where empno ="+where;
+	else if (gubun == 4)query = "delete from test where empno = " + setEmpno;
+	try{
+		connection = common.getConnection();
+		ps = connection.prepareStatement(query);
+		rs = ps.executeQuery();
+
+		if(gubun == 1) {
+			while(rs.next()) {
+				int empno = rs.getInt(1);//name
+				String ename = rs.getString(2);//3
+				String job = rs.getString(3);//4
+				
+				testDB_dto dto = new testDB_dto(empno, ename, job);
+				arr.add(dto);
+			}
+		}	
 	}catch(SQLException se){
 		System.out.println("getListAll() SQL query 오류" + query);
 	}catch(Exception e){
--- 20200730/src/main/Main.java
+++ 20200730/src/main/Main.java
@@ -15,12 +15,27 @@ public class Main {
 		
 		ArrayList<testDB_dto> arr = null;
 		int gubun = 0;
-		System.out.print("조회:1 입력:2  수정:3 삭제:4 종료:0 ");
-		gubun = sc.nextInt();
+		String blankString = "";
+		int blankValue = 0;
 		
 		do {
+			try {
+			System.out.println("조회:1 입력:2  수정:3 삭제:4 종료:0 ");
+			gubun = sc.nextInt();
+			//조회
 			if(gubun == 1) {
-				dao.selectTestDB();
+				//arr = dao.selectTestDB();
+				arr = dao.allControlTestDB(gubun, blankValue, blankString, blankString, blankValue);
+				System.out.println("Empno\tEname\tJob");
+				for (int i = 0; i < arr.size(); i++) {
+					System.out.print(arr.get(i).getEmpno()+"\t");
+					System.out.print(arr.get(i).getEname()+"\t");
+					System.out.print(arr.get(i).getJob()+"\n");
+				}
+				System.out.println("-----조회 완료-----");
+				gubun = -1;
+				
+			//입력	
 			}else if(gubun == 2) {
 				System.out.print("번호 입력 : ");
 				int no = sc.nextInt();
@@ -28,14 +43,37 @@ public class Main {
 				String name = sc.next();
 				System.out.print("직업 입력 : ");
 				String job = sc.next();
-				arr = dao.insertTestDB(no, name, job);
-				System.err.println("입력 완료");
+				//arr = dao.insertTestDB(no, name, job);
+				arr = dao.allControlTestDB(gubun, no, name, job, blankValue);
+				System.out.println("-----입력 완료-----");
+			
+			//수정	
 			}else if(gubun == 3) {
-				
+				System.out.print("수정할 번호 입력 : ");
+				int where = sc.nextInt();
+				System.out.print("번호 입력 : ");
+				int no = sc.nextInt();
+				System.out.print("이름 입력 : ");
+				String name = sc.next();
+				System.out.print("직업 입력 : ");
+				String job = sc.next();
+				//arr = dao.updateTestDB(no, name, job, where);
+				arr = dao.allControlTestDB(gubun, no, name, job, where);
+				System.out.println("-----수정 완료-----");
+			
+			//삭제	
 			}else if(gubun == 4) {
-				
+				System.out.print("삭제할 번호 입력 : ");
+				int no = sc.nextInt();
+				//arr = dao.deleteTestDB(no);
+				arr = dao.allControlTestDB(gubun, no, blankString, blankString, blankValue);
+				System.out.println("empno:"+no + " 삭제 완료");
 			}else if(gubun == 0) {
 				System.out.println("-----종료되었습니다-----");
+			}
+			}catch(Exception e) {
+				System.out.println("입력 오류");
+				gubun = 0;
 			}
 		}while(gubun != 0);
 
